c8f452331375f6d7c49cea26d15b36ab2b0af4cf
hayden <haybradshaw@gmail.com>
2019-07-15 15:34:47 -0500

    removed title from state

--- src/main/index.js
+++ src/main/index.js
@@ -6,7 +6,6 @@ export default class Main extends Component {
 		isPosting: true,
 		posts: [
 			{
-				title: 'Title',
 				author: 'author',
 				post:
 					'Ipsum reprehenderit id aliqua in deserunt. Esse sunt veniam culpa excepteur ex aliqua adipisicing irure occaecat enim ad consequat ullamco. Officia qui duis esse nisi cillum duis dolore et irure qui commodo quis. Incididunt eiusmod do fugiat voluptate voluptate eu proident consectetur aliquip reprehenderit sunt aliquip. Lorem occaecat velit velit quis ullamco culpa sunt anim enim reprehenderit enim exercitation sit qui. Ut sit voluptate eiusmod pariatur.'
