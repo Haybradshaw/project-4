170474688cff57bb935ffec29d3433d800deffa0
hayden <haybradshaw@gmail.com>
2019-07-15 16:56:31 -0500

    refactored page/app.js but import for sighnup is greyed out

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,21 +1,22 @@
 import React, { Component } from 'react';
 import { Switch, Route, BrowserRouter as Router } from 'react-router-dom'
+import userService from '../../utils/userService'
 import './App.css';
 import LoginPage from '../LoginPage/LoginPage'
 import SignupPage from '../SignupPage/SignupPage'
-import userService from '../../utils/userService'
-import tokenService from '../../utils/tokenService'
+
+
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      user: userService.getUser()
+      user: userService.getUser(),
     }
   }
 
   handleLogout = () => {
-    userService.logout();
+    userService.logOut();
     this.setState({ user: null });
   }
 
@@ -28,11 +29,11 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <Router>
-            <Switch>
-              <Route exact path='/signup' render={({ history }) =>
-                <SignupPage
-                  history={history}
-                  handleSignupOrLogin={this.handleSignupOrLogin}
+          <Switch>
+              <Route exact path='/' render={() =>
+                <SighupPage
+                  user={this.state.user}
+                  handleLogout={this.handleLogout}
                 />
               } />
               <Route exact path='/login' render={({ history }) =>
@@ -41,13 +42,13 @@ class App extends Component {
                   handleSignupOrLogin={this.handleSignupOrLogin}
                 />
               } />
-
             </Switch>
           </Router>
         </header>
       </div>
-    );
-  }
-}
-
-export default App;
+        );
+      }
+    }
+    
+    
+    export default App;
